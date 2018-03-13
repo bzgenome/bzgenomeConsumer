5256562155ae9960edf0a697e9658f21666fbfc5
irtaimal <ir.taimal10@uniandes.edu.co>
2018-03-12 23:18:00 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BzgComponentsModule } from 'bzg-components';
+import { AppComponent } from './app.component';
+const customGenomeConfiguration = {
+  "icons-sprite-path": "assets/sprite.svg",
+  "color-text-primary": "#555555",
+  "color-text-secondary": "#B7B7B7",
+  "color-button-primary": "#3e5c72",
+  "color-button-secondary": "transparent",
+  "color-header-primary": "#4285f4",
+  "color-header-secondary": "#4285f4",
+  "color-header-fill": "#ffffff",
+  "color-icon-header-primary": "#0039cb",
+  "color-icon-header-secondary": "#0039cb",
+  "color-icon-header-fill": "#ffffff",
+  "color-user-info": "#ffffff",
+  "color-avatar": "#dadeda",
+  "color-avatar-background": "#ffffff"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BzgComponentsModule.forRoot(customGenomeConfiguration)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
